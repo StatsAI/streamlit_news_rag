45b91dd11ff102e50233529ad6eade7891a32f3c
StatsAI <67183539+StatsAI@users.noreply.github.com>
2025-02-23 14:39:36 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import sqlite3
+
 import streamlit as st
 from unstructured.partition.html import partition_html
 from langchain.document_loaders import UnstructuredURLLoader
