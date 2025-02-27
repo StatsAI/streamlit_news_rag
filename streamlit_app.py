e62efaa623151ed7a3a017eec4a425f7fd6694f6
StatsAI <67183539+StatsAI@users.noreply.github.com>
2025-02-27 13:42:43 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ st.markdown(
         [data-testid=stSidebar] [data-testid=stImage]{
             text-align: center;
             display: block;
-            margin-left: 65px;
+            margin-left: 50px;
             margin-right: auto;
 	    margin-top: auto;
             width: 100%;
