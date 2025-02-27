e9c273b1b3dd9ca6be52cc06fc83899a98b8e82d
StatsAI <67183539+StatsAI@users.noreply.github.com>
2025-02-27 13:40:36 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -137,37 +137,37 @@ def load_gemini_model():
                                  google_api_key=gem_api_key)
     return llm
 
-# start = time.time()
-# links = pull_latest_links()
-# st.session_state['links'] = links
-# end = time.time()
-# diff = end - start
-# st.write(f"Latest links pulled in {round(diff,3)} seconds")
+start = time.time()
+links = pull_latest_links()
+st.session_state['links'] = links
+end = time.time()
+diff = end - start
+st.write(f"Latest links pulled in {round(diff,3)} seconds")
 
-# start = time.time()
-# docs = load_documents_parallel(links)
-# end = time.time()
-# diff = end - start
-# st.write(f"Content extracted from links in {round(diff,3)} seconds")
+start = time.time()
+docs = load_documents_parallel(links)
+end = time.time()
+diff = end - start
+st.write(f"Content extracted from links in {round(diff,3)} seconds")
 
-# start = time.time()
-# embedding_function = load_embedding_model()
-# end = time.time()
-# diff = end - start
-# st.write(f"Embedding model loaded in {round(diff,3)} seconds")
+start = time.time()
+embedding_function = load_embedding_model()
+end = time.time()
+diff = end - start
+st.write(f"Embedding model loaded in {round(diff,3)} seconds")
 
-# #st.write("Embedding model loaded!")
-# start = time.time()
-# vectorstore = load_vector_database(embedding_function, docs)
-# end = time.time()
-# diff = end - start
-# st.write(f"Vector database loaded in {round(diff,3)} seconds")
+#st.write("Embedding model loaded!")
+start = time.time()
+vectorstore = load_vector_database(embedding_function, docs)
+end = time.time()
+diff = end - start
+st.write(f"Vector database loaded in {round(diff,3)} seconds")
 
-# start = time.time()
-# llm = load_gemini_model()
-# end = time.time()
-# diff = end - start
-# st.write(f"Gemini model loaded in {round(diff,3)} seconds")
+start = time.time()
+llm = load_gemini_model()
+end = time.time()
+diff = end - start
+st.write(f"Gemini model loaded in {round(diff,3)} seconds")
 
 # Textbox for user query
 #query = st.text_input("Enter your query:")
@@ -186,11 +186,11 @@ if st.sidebar.button('Get latest links'):
 	diff = end - start
 	st.write(f"Content extracted from links in {round(diff,3)} seconds")
 	
-	start = time.time()
-	embedding_function = load_embedding_model()
-	end = time.time()
-	diff = end - start
-	st.write(f"Embedding model loaded in {round(diff,3)} seconds")
+	# start = time.time()
+	# embedding_function = load_embedding_model()
+	# end = time.time()
+	# diff = end - start
+	# st.write(f"Embedding model loaded in {round(diff,3)} seconds")
 	
 	#st.write("Embedding model loaded!")
 	start = time.time()
@@ -199,11 +199,11 @@ if st.sidebar.button('Get latest links'):
 	diff = end - start
 	st.write(f"Vector database loaded in {round(diff,3)} seconds")
 	
-	start = time.time()
-	llm = load_gemini_model()
-	end = time.time()
-	diff = end - start
-	st.write(f"Gemini model loaded in {round(diff,3)} seconds")
+	# start = time.time()
+	# llm = load_gemini_model()
+	# end = time.time()
+	# diff = end - start
+	# st.write(f"Gemini model loaded in {round(diff,3)} seconds")
 
 if query:    
     # Query the vector database
