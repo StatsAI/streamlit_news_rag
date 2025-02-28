90c83c1eea97cc1facaa851e4ef72d60635cf798
StatsAI <67183539+StatsAI@users.noreply.github.com>
2025-02-27 20:09:00 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -224,7 +224,7 @@ if query:
 	    source = doc.metadata
 	    result = chain.invoke([doc])
 	    st.write(result['output_text'])
-	    #string = str(source)
-	    st.write(str(source.values), unsafe_allow_html=True)
+	    st.write(type(source))
+	    st.write(str(source), unsafe_allow_html=True)
 	    st.write(source)
 	    st.write('')
