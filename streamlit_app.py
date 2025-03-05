9309b857ff923837fb8ab3f57327de4ec35dde7e
StatsAI <67183539+StatsAI@users.noreply.github.com>
2025-03-05 16:20:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -206,7 +206,7 @@ if st.sidebar.button('Summarize Articles') or query:
 st.sidebar.write("The link cache is updated once a day. Pressing the below button bypasses this, at the cost of a minute to download/load the vector database") 
 
 if st.sidebar.button('Clear cache & get latest links!'):	
-	st.session_state[query_text] = ""
+	st.session_state["query_text"] = ""
 	pull_latest_links.clear()
 	load_documents_parallel.clear()
 	load_vector_database.clear()	
