6f7433bb11469605563e0254011e56bc3282521f
StatsAI <67183539+StatsAI@users.noreply.github.com>
2025-03-05 16:48:27 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -203,7 +203,6 @@ if st.session_state['cache_cleared']:
     st.session_state['cache_cleared'] = False  # Reset the flag
     default_query = "Trump"
     st.session_state['last_query'] = default_query
-    st.session_state['query_text'] = default_query #Set the textbox value.
     query_docs = vectorstore.similarity_search(default_query, k=5)
     chain = load_summarize_chain(llm, chain_type="stuff")
 
